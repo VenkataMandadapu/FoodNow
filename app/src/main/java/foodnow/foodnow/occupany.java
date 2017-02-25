4967a4bea12fbcb1b30f1d5678ded32dad291382
VineethThumma <vineethreddy.2704@gmail.com>
2017-02-24 20:03:00 -0500

    "Add Debug messages in Activity life cycle methods"

--- app/src/main/java/foodnow/foodnow/occupany.java
+++ app/src/main/java/foodnow/foodnow/occupany.java
@@ -5,12 +5,14 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
 public class occupany extends AppCompatActivity {
-
+    private final String LOG_TAG = getClass().getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(LOG_TAG,"In Occupancy On Create");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_occupany);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -24,6 +26,29 @@ public class occupany extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.d(LOG_TAG,"In occupancy On Resume");
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        Log.d(LOG_TAG,"In occupancy On Pause");
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        Log.d(LOG_TAG,"In occupancy On Stop");
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.d(LOG_TAG,"In occupancy On Destroy");
     }
 
 }

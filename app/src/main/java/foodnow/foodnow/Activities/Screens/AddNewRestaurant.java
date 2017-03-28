480338f5107afac402247d08ad6312cc454e7ad3
VineethThumma <vineethreddy.2704@gmail.com>
2017-03-27 23:26:44 -0400

    "Menu changes" Signed-off-by: VineethThumma <vineethreddy.2704@gmail.com>

--- app/src/main/java/foodnow/foodnow/Activities/Screens/AddNewRestaurant.java
+++ app/src/main/java/foodnow/foodnow/Activities/Screens/AddNewRestaurant.java
@@ -15,7 +15,6 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-import foodnow.foodnow.Activities.Menu.MenuActivity;
 import foodnow.foodnow.Activities.Sessions.SessionManager;
 import foodnow.foodnow.DatabaseModels.RestaurantDB;
 import foodnow.foodnow.DatabaseModels.RestaurantStatusDB;
